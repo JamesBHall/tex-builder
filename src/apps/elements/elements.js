6b5b92337409e2f438a615f76548a1738a3ebdb9
Jimmy Hall <jamesb.hall@gmail.com>
2021-09-16 16:06:41 -0400

    Updates.

--- src/apps/elements/elements.js
+++ src/apps/elements/elements.js
@@ -1,31 +1,35 @@
 function convert() {
+    const { ipcRenderer } = require('electron');
+    
     var input = document.getElementById("text-input").value.trim();
     var display = document.getElementById("render");
     var renderOptions = {em: 16, ex:8, containerWidth: 589, display: true, scale: 1, lineWidth: 1000};
     var element = MathJax.tex2svg(input, renderOptions);
     display.innerHTML = element.innerHTML;
-}
+
+    ipcRenderer.send('cache-render', export_svg_data());
+}    
 render_button = document.getElementById("render-button");
 render_button.addEventListener('click', convert, false);
 
 function export_svg() {
-	var input = document.getElementById("text-input").value.trim();
-	var renderOptions = {em: 16, ex:8, scale: 1};
-	var element = MathJax.tex2svg(input, renderOptions);
-	var ele;
-	for (child in element.children) {
-	    ele = element.children[child];
-	    if (ele.nodeName == 'svg') {
-		var svg = ele
-	    }
+    var input = document.getElementById("text-input").value.trim();
+    var renderOptions = {em: 16, ex: 8, scale: 1};
+    var element = MathJax.tex2svg(input, renderOptions);
+    var ele;
+    for (child in element.children) {
+	ele = element.children[child];
+	if (ele.nodeName == 'svg') {
+	    var svg = ele
 	}
-	var height = Math.floor(svg.viewBox.baseVal.height);
-	var width = Math.floor(svg.viewBox.baseVal.width);
-	svg.setAttribute('width', width);
-	svg.setAttribute('height', height);
-	var svgBlob = new Blob([svg.outerHTML], {type: "image/svg+xml;charset=utf-8"});
-	saveAs(svgBlob, "output.svg")
     }
+    var height = Math.floor(svg.viewBox.baseVal.height);
+    var width = Math.floor(svg.viewBox.baseVal.width);
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+    var svgBlob = new Blob([svg.outerHTML], {type: "image/svg+xml;charset=utf-8"});
+    saveAs(svgBlob, "output.svg")
+}
 save_button = document.getElementById("export-svg-button");
 save_button.addEventListener('click', export_svg, false);
 
@@ -89,8 +93,16 @@ function toggle_preview() {
 }
 toggle_preview_button = document.getElementById("toggle-preview-button");
 toggle_preview_button.addEventListener('click', toggle_preview, false);
-  
-function export_svg() {
+
+function export_svg_data() {
+    const { ipcRenderer } = require('electron');
+    //ipcRenderer.send('request-export-size');
+    long_dim = 0;
+    long_dim = ipcRenderer.sendSync('request-export-size');
+    console.log("Long dim");
+    console.log(long_dim);
+		  
+    
     var input = document.getElementById("text-input").value.trim();
     var renderOptions = {em: 16, ex:8, scale: 1};
     var element = MathJax.tex2svg(input, renderOptions);
@@ -103,8 +115,25 @@ function export_svg() {
     }
     var height = Math.floor(svg.viewBox.baseVal.height);
     var width = Math.floor(svg.viewBox.baseVal.width);
-    svg.setAttribute('width', width);
-    svg.setAttribute('height', height);
+
+    max_dim = Math.max(height, width);
+    if (long_dim == 0) {
+	scale = 1.0;
+    } else {
+	scale = long_dim / max_dim;
+    }
+
+    console.log("Width:")
+    console.log(width)
+    console.log("Height:")
+    console.log(height)
+    console.log("Max dim:")
+    console.log(max_dim)
+    console.log("Scaler:")
+    console.log(scale)
+
+    svg.setAttribute('width', Math.floor(width*scale));
+    svg.setAttribute('height', Math.floor(height*scale));
     return svg.outerHTML;
 }
 
@@ -113,6 +142,6 @@ const renderElement = document.getElementById('render');
 
 renderElement.addEventListener('dragstart', (event) => {
     event.preventDefault();
-    svg_data = export_svg();
-    ipcRenderer.send('on-render-drag-start', svg_data, false)
+    svg_data = export_svg_data();
+    ipcRenderer.send('on-render-drag-start', svg_data);
 });
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
 const fs = require('fs');
 const svg2png = require('svg2png');
 
+var drag_format = 'SVG';
+var export_size = 0;
+
 function createWindow() {
     const win = new BrowserWindow({
 	window: 800,
@@ -13,7 +16,6 @@ function createWindow() {
 	}
     })
     win.loadFile('index.html')
-    //win.webContents.openDevTools()
 
     var menu = Menu.buildFromTemplate([
 	{
@@ -42,6 +44,39 @@ function createWindow() {
 	    ]
 	},
 	{
+	    label: 'Export Settings',
+	    submenu: [
+		{label: 'Drag Format',
+		 submenu: [
+		     {label: 'SVG',
+		      type: 'radio',
+		      checked: true,
+		      click() {drag_format = 'SVG';}},
+		     {label: 'PNG',
+		      type: 'radio',
+		      checked: false,
+		      click() {drag_format = 'PNG';}}
+		 ]
+		},
+		{label: 'Export Size',
+		 submenu: [
+		     {'label': '100',
+		      'type': 'radio',
+		      checked: false,
+		      click() {export_size = 100;}},
+		     {'label': '250',
+		      'type': 'radio',
+		      checked: false,
+		      click() {export_size = 250;}},
+		     {'label': 'Full',
+		      'type': 'radio',
+		      checked: true,
+		      click() {export_size = 0;}}
+		 ]
+		},
+	    ]
+	},
+	{
 	    label: 'Dev Tools',
 	    submenu: [
 		{role: 'toggleDevTools'}
@@ -51,6 +86,12 @@ function createWindow() {
     Menu.setApplicationMenu(menu)
 }
 app.whenReady().then(createWindow)
+
+ipcMain.on('request-export-size', (event) => {
+    console.log('Export size requested');
+    //event.reply('reply-export-size', export_size);
+    event.returnValue = export_size;
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -67,8 +108,10 @@ app.on('activate', () => {
 ipcMain.on('request-save-png', (event, arg) => {
 
     dialog.showSaveDialog(null, {defaultPath: "output.png"}).then(result => {
-	svg2png(arg).then(
-	    buffer => {fs.writeFileSync(result.filePath, buffer)})
+	if (!result.canceled) {
+	    svg2png(arg).then(
+		buffer => {fs.writeFileSync(result.filePath, buffer)})
+	}
     }).catch(err => {
 	console.log(err)
     })
@@ -78,37 +121,72 @@ ipcMain.on('request-save-png', (event, arg) => {
 ipcMain.on('request-save-text', (event, arg) => {
 
     dialog.showSaveDialog(null, {defaultPath: "equation.txt"}).then(result => {
-	fs.writeFileSync(result.filePath, arg)
+	if (!result.canceled) {
+	    fs.writeFileSync(result.filePath, arg)
+	}
     }).catch(err => {
 	console.log(err)
     })
-
-    console.log(arg)
 
 });
 
 ipcMain.on('request-load-text', (event, arg) => {
 
     dialog.showOpenDialog(null, {}).then(result => {
-	console.log(result);
-	fs.readFile(result.filePaths[0], 'utf8', (err, data) => {
-	    if (err) throw err;
-	    event.reply('reply-loaded-text', data);
-	}).catch(err => {
-	    console.log(err);
-	});
+	if (!result.canceled) {
+	    fs.readFile(result.filePaths[0], 'utf8', (err, data) => {
+		if (err) throw err;
+		event.reply('reply-loaded-text', data);
+	    }).catch(err => {
+		console.log(err);
+	    });
+	}
     });
 
 });
+
+ipcMain.on('cache-render', (event, data) => {
+    const appPath = app.getAppPath();
+    const baseRenderPath = "./temp_render";
+    tempRenderPath = path.join(appPath, baseRenderPath);
+    
+    temp_png_loc = path.join(tempRenderPath, "temp_render.png");
+    svg2png(data).then(
+	buffer => {fs.writeFileSync(temp_png_loc, buffer);}
+    );
+
+    temp_svg_loc = path.join(tempRenderPath, "temp_render.svg");
+    fs.writeFileSync(temp_svg_loc, data);
+})
 
 ipcMain.on('on-render-drag-start', (event, data) => {
     const appPath = app.getAppPath();
     const baseRenderPath = "./temp_render";
     tempRenderPath = path.join(appPath, baseRenderPath);
-    temp_loc = path.join(tempRenderPath, "temp_render.svg")
-    fs.writeFileSync(temp_loc, data)
-    event.sender.startDrag({
-	file: temp_loc,
-	icon: "./assets/icons/placeholder.png"
-    })
+    
+    temp_png_loc = path.join(tempRenderPath, "temp_render.png");
+    temp_svg_loc = path.join(tempRenderPath, "temp_render.svg");
+    
+    if (drag_format === "PNG") {
+	//temp_loc = path.join(tempRenderPath, "temp_render.png");
+	/*
+	svg2png(data).then(
+	    buffer => {fs.writeFileSync(temp_loc, buffer);
+		       event.sender.startDrag({
+			   file: temp_loc,
+			   icon: "./assets/icons/preview_icon.png"});
+		      })
+	*/
+	event.sender.startDrag({
+	    file: temp_png_loc,
+	    icon: "./assets/icons/preview_icon.png"});
+    };
+	  
+    if (drag_format == "SVG") {
+	//temp_loc = path.join(tempRenderPath, "temp_render.svg");
+	//fs.writeFileSync(temp_loc, data);
+	event.sender.startDrag({
+	    file: temp_svg_loc,
+	    icon: "./assets/icons/preview_icon.png"});
+    };
 })
